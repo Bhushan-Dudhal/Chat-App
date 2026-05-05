4dee798edce007e46ac8a936b74fa384c078e2e0
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2026-05-05 09:15:34 +0530

    firestore service

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,3 +1,7 @@
+import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
+
+import 'package:chat_app/models/friend_request_model.dart';
+import 'package:chat_app/models/notification_model.dart';
 import 'package:chat_app/models/user_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -63,9 +67,122 @@ class FirestoreService {
   Future<void> updateUser(UserModel user) async {
     try {
       await _firestore.collection('users').doc(user.id).update(user.toMap());
-      
     } catch (e) {
       throw Exception('Failed To Update User');
+    }
+  }
+
+  Stream<List<UserModel>> getAllUsersStream() {
+    return _firestore
+        .collection('users')
+        .snapshots()
+        .map(
+          (snapshot) => snapshot.docs
+              .map((doc) => UserModel.fromMap(doc.data()))
+              .toList(),
+        );
+  }
+
+  //friend request collection
+
+  Future<Void> sendFriendRequest(FriendRequestModel request) async {
+    try {
+      await _firestore
+          .collection('friendRequests')
+          .doc(request.id)
+          .set(request.toMap());
+
+      String notificationId =
+          'friend_request_${request.senderId}_${request.receiverId}_${DateTime.now().microsecondsSinceEpoch}';
+
+      await createNotification(
+        NotificationModel(
+          id: notificationId,
+          userId: request.receiverId,
+          title: 'New Friend Request',
+          body: 'You have received a new friend request',
+          type: NotificationType.friendRequest,
+          data: {'senderId': request.senderId, 'requestId': request.id},
+          createdAt: DateTime.now(),
+        ),
+      );
+    } catch (e) {
+      throw Exception('Failed To send Friend Request: ${e.toString()}');
+    }
+  }
+
+  Future<void> cancelFriendRequest(String requestId) async {
+    try {
+      DocumentSnapshot requestDoc = await _firestore
+          .collection('friendRequests')
+          .doc(requestId)
+          .get();
+
+      if (requestDoc.exists) {
+        FriendRequestModel request = FriendRequestModel.fromMap(
+          requestDoc.data() as Map<String, dynamic>,
+        );
+        await _firestore.collection('friendRequests').doc(requestId).delete();
+
+        await deleteNotificationByTypeAndUser(
+          request.receiverId,
+          NotificationType.friendRequest,
+          request.senderId,
+        );
+      }
+    } catch (e) {
+      throw Exception('Failed To Cancel Friend Request ${e.toString()}');
+    }
+  }
+
+  Future<void> respondToFriendRequest(
+    String requestId,
+    FriendRequestStatus status,
+  ) async {
+    try {
+      await _firestore.collection('friendRequests').doc(requestId).update({
+        'status': status.name,
+        'respondedAt': DateTime.now().microsecondsSinceEpoch,
+      });
+      DocumentSnapshot requestDoc = await _firestore
+          .collection('friendRequests')
+          .doc(requestId)
+          .get();
+
+      if (requestDoc.exists) {
+        FriendRequestModel request = FriendRequestModel.fromMap(
+          requestDoc.data() as Map<String, dynamic>,
+        );
+
+        if (status == FriendRequestStatus.acceptd) {
+          await createFriendShip(request.senderId, request.receiverId);
+
+          await createNotification(
+            NotificationModel(
+              id: DateTime.now().microsecondsSinceEpoch.toString(),
+              userId: request.senderId,
+              title: 'Friend Request Accepted',
+              body: 'Your friend request has been accepted',
+              type: NotificationType.friendRequestAccepted,
+              data: {'userId': request.receiverId},
+              createdAt: DateTime.now(),
+            ),
+          );
+
+          await _removeNotificationForCancelledRequest(
+            request.receiverId,
+            request.senderId,
+
+          );
+        }
+        else if(status == FriendRequestStatus.declined){
+          await createNotidication(
+            NotificationModel(id: , userId: userId, title: title, body: body, type: type, createdAt: createdAt)
+          )
+        }
+      }
+    } catch (e) {
+      throw Exception('Failed To Respond To Friend Request: ${e.toString()}');
     }
   }
 }
